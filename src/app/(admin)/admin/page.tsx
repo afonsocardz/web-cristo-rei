59b4162a2a13209cac9f4b429616efae9af82fa5
afonso.cruz <afonso.cruz@netcreator.com.br>
2024-01-22 12:57:54 -0300

    chore: add form button

--- src/app/(admin)/admin/page.tsx
+++ src/app/(admin)/admin/page.tsx
@@ -1,11 +1,13 @@
 "use client";
 
+import Button from "@/components/ui/Button/Button";
 import Card from "@/components/ui/Card/Card";
+import Text from "@/components/ui/Text/Text";
 import TextField from "@/components/ui/TextField/TextField";
 
 export default function Admin() {
   return (
-    <main className="w-full">
+    <main className="w-full h-full">
       <div className="flex justify-center items-center w-full h-full">
         <Card
           sx={{
@@ -18,8 +20,10 @@ export default function Admin() {
           }}
           elevation={4}
         >
+          <Text align="center">Painel Administrador</Text>
           <TextField label={"E-mail"} size="small" />
           <TextField label={"Senha"} size="small" />
+          <Button variant="contained">Entrar</Button>
         </Card>
       </div>
     </main>
